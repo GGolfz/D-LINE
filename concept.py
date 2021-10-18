eec5cf657ddb93aef725f53f1f915dc41def652a
GGolfz <wisarutgolf11@gmail.com>
2021-10-18 11:37:12 +0700

    add: log to more than 1 terminal :sparkles:

--- concept.py
+++ concept.py
@@ -1,30 +1,33 @@
 import time
 from threading import Timer
+from datetime import datetime
+import socket
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    OKBLUE = '\033[94m'
+    OKMAGENTA = '\033[95m'
+    OKCYAN = '\033[96m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 class Process:
-    def __init__(self,pid):
+    def __init__(self,pid,pipe):
         self.pid = pid
         self.vector_time = {}
         self.buffer_list = []
         self.lock_buffer = False
         self.address = {pid:self}
+        self.pipe = pipe
         if pid == 1:
             self.color = bcolors.OKBLUE
         elif pid == 2:
             self.color = bcolors.OKCYAN
         elif pid == 3:
             self.color = bcolors.OKGREEN
+        else:
+            self.color = bcolors.OKMAGENTA
     def connect(self,peer):
-        print(str(self.pid) + " and " + str(peer.pid) + " are connected at time " + time.ctime() )
+        log = bcolors.WARNING + datetime.now().strftime("%H:%M:%S") + " | " + str(self.pid) + " and " + str(peer.pid) + " are connected" + '\n'
+        print(log,end='')
         self.address[peer.pid]=peer
         self.vector_time[peer.pid] = {self.pid: 0, peer.pid: 0}
         peer.address[self.pid] = self
@@ -45,9 +48,16 @@ class Process:
             self.send_message_by_type(target,5,type,content)
             if self.pid == 2 and target.pid == 1:
                 self.send_message_by_type(self.address[3],5,type,content)
+        time.sleep(0.5)
+    def get_send_log(self,target,content,type):
+        return self.color + datetime.now().strftime("%H:%M:%S") + " | " + str(self.pid) + " send " + type + " message (" + content + ") to " + str(target.pid) + " (sequence time = " + str(self.vector_time[target.pid][self.pid]) + ")" + bcolors.ENDC + '\n'
+    def get_receive_log(self,message):
+        return self.color + datetime.now().strftime("%H:%M:%S") + " | " + str(self.pid) + " receive " + message['type'] + " message (" + message['content'] + ") from " + str(message['pid']) + " (sequence time = " + str(self.vector_time[message['pid']][message['pid']]) + ")" + bcolors.ENDC + '\n'
     def send_message_by_type(self,target,delay,type,content):
         self.vector_time[target.pid][self.pid] += 1
-        print(self.color + str(self.pid) + " send the "+type+" message to "+ str(target.pid) + " at time "+ time.ctime() + '(sequence = '+ str(self.vector_time[target.pid][self.pid]) +')' + bcolors.ENDC)
+        log = self.get_send_log(target,content,type)
+        print(log,end='')
+        self.pipe.send(log.encode())
         message = {'pid':self.pid,'content':content,'vector_time':self.vector_time[target.pid].copy(),'type': type}
         Timer(delay,target.receive_message,[message]).start()
     def receive_message(self,message):
@@ -67,7 +77,9 @@ class Process:
                     self.buffer_list.append(message)
                     return False
         self.vector_time[message['pid']] = vector_temp
-        print(self.color + str(self.pid) + " receive the "+ message['type'] + " message from " + str(message['pid'])+ " : " + message['content'] + " at time "+ time.ctime() +  '(sequence = '+ str(self.vector_time[message['pid']][message['pid']])  +')' + bcolors.ENDC)
+        log = self.get_receive_log(message)
+        print(log,end = '')
+        self.pipe.send(log.encode())
         # If process 2 then forward to process 3
         if self.pid == 2:
             self.send_message(self.address[3],message['type'],message['content'])
@@ -81,28 +93,46 @@ class Process:
                 break
             self.receive_message(self.buffer_list.pop(0))
 def main():
-    p1 = Process(1)
-    p2 = Process(2)
-    p3 = Process(3)
+    socketp1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socketp1.connect(('localhost',5001))
+    socketp2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socketp2.connect(('localhost',5002))
+    socketp3 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socketp3.connect(('localhost',5003))
+    socketp4 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socketp4.connect(('localhost',5004))
+    p1 = Process(1,socketp1)
+    p2 = Process(2,socketp2)
+    p3 = Process(3,socketp3)
+    p4 = Process(4,socketp4)
     p1.connect(p2)
+    p1.connect(p4)
     p2.connect(p3)
+    p2.connect(p4)
     p1.send_message(p2,'text','text 1')
     p1.send_message(p2,'video','video 1')
     p1.send_message(p2,'image','image 1')
     p1.send_message(p2,'image','image 2')
+    p1.send_message(p4,'image','image 3')
+    p1.send_message(p4,'image','image 4')
     p1.send_message(p2,'text','text 2')
     p1.send_message(p2,'video','video 2')
     p2.send_message(p1,'video','video 1 from p2')
     p1.send_message(p2,'video','video 3')
     p1.send_message(p2,'text','text 3')
     p1.send_message(p2,'text','text 4')
-    p1.send_message(p2,'image','image 3')
+    p1.send_message(p2,'image','image 5')
     p1.send_message(p2,'text','text 5')
     p1.send_message(p2,'text','text 6')
     p1.send_message(p2,'video','video 4')
+    p4.send_message(p1,'text','text 7')
+    p4.send_message(p2,'text','text 8')
     time.sleep(20)
     print(p1.buffer_list)
     print(p2.buffer_list)
     print(p3.buffer_list)
+    socketp1.close()
+    socketp2.close()
+    socketp3.close()
 if __name__ == '__main__':  
     main()
